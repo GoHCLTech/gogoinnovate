a0ee37b0bc5040791f5bd863583ffbc7ea651dd3
Kurt Wolf <joemagnum1@hotmail.com>
2016-02-09 12:53:33 -0500

    fixed php validation

--- send_form_email.php
+++ send_form_email.php
@@ -38,11 +38,7 @@ if(isset($_POST['email'])) {
  
         !isset($_POST['last_name']) ||
  
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
- 
-        !isset($_POST['comments'])) {
+        !isset($_POST['email'])) {
  
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
  
@@ -55,13 +51,8 @@ if(isset($_POST['email'])) {
     $last_name = $_POST['last_name']; // required
  
     $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
- 
-    $comments = $_POST['comments']; // required
- 
      
- 
+
     $error_message = "";
  
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -83,12 +74,6 @@ if(isset($_POST['email'])) {
   if(!preg_match($string_exp,$last_name)) {
  
     $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
  
   }
  
@@ -117,10 +102,6 @@ if(isset($_POST['email'])) {
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
  
     $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
  
      
  
